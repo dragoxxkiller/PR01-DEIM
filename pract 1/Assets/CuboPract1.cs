c6799cf8aad8a8a7c2075ded06768b25abb315ca
Natalia Gomez <marinat306@gmail.com>
2021-09-24 16:44:51 +0200

    Métodos y Corrutinas----- b)
    He echo primero el b) antes que el a) porque no lo sabia hacer bien el a)

--- pract 1/Assets/CuboPract1.cs
+++ pract 1/Assets/CuboPract1.cs
@@ -7,22 +7,28 @@ public class CuboPract1 : MonoBehaviour
 
     public string mensaje = "Houston tenemos un problema";
     public string mensaje2 = "Te me bajas tres tonitos";
+    public string nombrecito = "Eustaquio";
     public int numerito = 20;
     public float numero = 1.23f;
     public bool Alive = true;
-     int n = 0;
+     int n = 2;
     int m = 10;
     int k = 14;
+    string Saludo;
+
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
 
-        while (n != 5 )
-        {
-            n = Random.Range(-2000,2000);
-            print(n);
-        }
+        int resultado = multiplicar(2, 20);
+        print(resultado);
+       
+        
 
       
     }
